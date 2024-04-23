f8767ce107a6546dbe97f150d6c82f1b9061b8e7
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2024-04-23 20:58:08 +0530

    HTTP Server - Refactor the code and add 404 Response

--- HTTPserverFromScratch/server.cpp
+++ HTTPserverFromScratch/server.cpp
@@ -8,69 +8,90 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+// Function to check if the HTTP request path is empty
+bool isEmptyPath(const std::string& request) {
+  // Find the position of the first '/' character in the request
+  size_t rootPath = request.find('/');
+  // Check if the character immediately after '/' is a space, indicating an empty path
+  return isspace(request[rootPath + 1]);
+}
+
 int main(int argc, char **argv) {
-    
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    
-    if (server_fd < 0) {
-     std::cerr << "Failed to create server socket\n";
-     return 1;
-    }
-   
-    // Since the tester restarts your program quite often, setting REUSE_PORT
-    // ensures that we don't run into 'Address already in use' errors
-    int reuse = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-      std::cerr << "setsockopt failed\n";
-      return 1;
-    }
-   
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(4221);
-   
-    if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-      std::cerr << "Failed to bind to port 4221\n";
-      return 1;
-    }
-   
-    int connection_backlog = 5;
-    if (listen(server_fd, connection_backlog) != 0) {
-      std::cerr << "listen failed\n";
-      return 1;
-    }
-   
-    struct sockaddr_in client_addr;
-    int client_addr_len = sizeof(client_addr);
-   
-    std::cout << "Waiting for a client to connect...\n";
-   
-    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-    if(client_fd < 0) {
-      std::cout << "Failed to accept client\n";
-      return 1;
-    }
-    std::cout << "Client connected\n";
+  // Create a socket for the server to listen for incoming connections
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0) {
+    std::cerr << "Failed to create server socket\n";
+    return 1;
+  }
+  
+  // Enable reuse of the port to avoid "Address already in use" errors
+  int reuse = 1;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
+    std::cerr << "setsockopt failed\n";
+    return 1;
+  }
+  
+  // Define the server address structure
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(4221);
+  
+  // Bind the server socket to the specified address and port
+  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+    std::cerr << "Failed to bind to port 4221\n";
+    return 1;
+  }
+  
+  // Listen for incoming connections on the server socket
+  int connection_backlog = 5;
+  if (listen(server_fd, connection_backlog) != 0) {
+    std::cerr << "listen failed\n";
+    return 1;
+  }
+  
+  // Accept incoming connection from a client
+  struct sockaddr_in client_addr;
+  socklen_t client_addr_len = sizeof(client_addr);
+  std::cout << "Waiting for a client to connect...\n";
+  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+  if (client_fd < 0) {
+    std::cerr << "Failed to accept client\n";
+    return 1;
+  }
+  std::cout << "Client connected\n";
 
-    unsigned char buffer[1024];
-    ssize_t nbytes = recv(client_fd, buffer, 1024, 0);
-    if(nbytes < 0) {
-      std::cout << "Failed to recv from client\n";
-      close(client_fd);
-      return 1;
-    }
+  // Buffer to store received data from client
+  char buffer[1024] = {0};
+  
+  // Receive data from the client
+  ssize_t nbytes = recv(client_fd, buffer, 1024, 0);
+  if (nbytes < 0) {
+    std::cerr << "Failed to recv from client\n";
+    close(client_fd);
+    return 1;
+  } 
 
-    // Send HTTP Response - 200
-    std::string response {"HTTP/1.1 200 OK\r\n\r\n"};
-    nbytes = send(client_fd, response.c_str(), response.length(), 0);
-    if(nbytes < 0) {
-      std::cout << "Failed to connect to client\n";
-      close(client_fd);
-      return 1;
-    }
+  // Prepare HTTP response based on request path
+  std::string response = "HTTP/1.1 404 Not Found\r\n\r\n";
+  if (isEmptyPath(buffer)) {
+    // If request path is empty, send a 200 OK response
+    response = "HTTP/1.1 200 OK\r\n\r\n";
+  }
 
-    close(server_fd);
+  // Send HTTP response back to the client
+  nbytes = send(client_fd, response.c_str(), response.length(), 0);
+  if (nbytes < 0) {
+    std::cerr << "Failed to send response to client\n";
+    close(client_fd);
+    return 1;
+  }
+  
+  // Close client socket
+  close(client_fd);
+  
+  // Close server socket
+  close(server_fd);
 
-    return 0;
+  return 0;
 }
