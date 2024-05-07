1fae36934cb4a2c6a596db5a34dee899ccb0cf79
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2024-05-07 21:16:30 +0530

    Handle multiple connections

--- HTTPserverFromScratch/server.cpp
+++ HTTPserverFromScratch/server.cpp
@@ -9,61 +9,18 @@
 #include <netdb.h>
 #include <vector>
 #include <sstream>
+#include <thread>
+#include <pthread.h>
 
-int main(int argc, char **argv) {
-  // Create a socket for the server to listen for incoming connections
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-    std::cerr << "Failed to create server socket\n";
-    return 1;
-  }
-  
-  // Enable reuse of the port to avoid "Address already in use" errors
-  int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "setsockopt failed\n";
-    return 1;
-  }
-  
-  // Define the server address structure
-  struct sockaddr_in server_addr;
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(4221);
-  
-  // Bind the server socket to the specified address and port
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 4221\n";
-    return 1;
-  }
-  
-  // Listen for incoming connections on the server socket
-  int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
-    return 1;
-  }
-  
-  // Accept incoming connection from a client
-  struct sockaddr_in client_addr;
-  socklen_t client_addr_len = sizeof(client_addr);
-  std::cout << "Waiting for a client to connect...\n";
-  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-  if (client_fd < 0) {
-    std::cerr << "Failed to accept client\n";
-    return 1;
-  }
-  std::cout << "Client connected\n";
-
-  if(client_fd != -1) {
-      char buffer[1024] = {0};
+void handleConnection(int client_fd) {
+    char buffer[1024];
 
       // Receive the message
       ssize_t nbytes = recv(client_fd, buffer, 1024, 0);
       if(nbytes < 0) {
         std::cerr << "Failed to recv from client\n";
         close(client_fd);
-        return 1;
+        return;
       } 
       
       // Parse the message
@@ -123,8 +80,59 @@ int main(int argc, char **argv) {
       if(nbytes < 0) {
         std::cerr << "Failed to connect to client\n";
         close(client_fd);
-        return 1;
+        return;
       }
+
+      close(client_fd);
+      std::cout << "Connection closed\n";
+}
+
+int main(int argc, char **argv) {
+  // Create a socket for the server to listen for incoming connections
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0) {
+    std::cerr << "Failed to create server socket\n";
+    return 1;
+  }
+  
+  // Enable reuse of the port to avoid "Address already in use" errors
+  int reuse = 1;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
+    std::cerr << "setsockopt failed\n";
+    return 1;
+  }
+  
+  // Define the server address structure
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(4221);
+  
+  // Bind the server socket to the specified address and port
+  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+    std::cerr << "Failed to bind to port 4221\n";
+    return 1;
+  }
+  
+  // Listen for incoming connections on the server socket
+  int connection_backlog = 5;
+  if (listen(server_fd, connection_backlog) != 0) {
+    std::cerr << "listen failed\n";
+    return 1;
+  }
+  
+  // Accept incoming connection from a client
+  struct sockaddr_in client_addr;
+  socklen_t client_addr_len = sizeof(client_addr);
+  std::cout << "Waiting for a client to connect...\n";
+  while(true) {
+    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+    if(client_fd != -1) {
+      std::cout << "Client connected\n";
+      std::thread(handleConnection, client_fd).detach();
+      //std::thread thread_obj(handleConnection, client_fd);
+      //thread_obj.detach();
+    }
   }
   // Close server socket
   close(server_fd);
