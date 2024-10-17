2f9acb20de1f1139c33b27e8d98d1c94d8a8752e
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2024-10-17 22:44:58 +0530

    Matrix calcularor

--- FocusOnCppTopics/MatrixCalculator/Matrix.cpp
+++ FocusOnCppTopics/MatrixCalculator/Matrix.cpp
@@ -1,40 +1,5 @@
 #include "Matrix.hpp"
 
-// Constructor
-template <size_t Rows, size_t Cols>
-Matrix<Rows, Cols>::Matrix() {
-    for(auto& row : data) {
-        row.fill(0);
-    }
-}
-
-// Set element
-template <size_t Rows, size_t Cols>
-void Matrix<Rows, Cols>::set(size_t row, size_t col, int value) {
-    if(row >= Rows || col >= Cols) {
-        throw std::out_of_range("Index out of range\n");
-    }
-    data[row][col] = value;
-}
-
-// Get element
-template <size_t Rows, size_t Cols>
-int Matrix<Rows, Cols>::get(size_t row, size_t col) const {
-    if(row >= Rows || col >= Cols) {
-        throw std::out_of_range("Index out of range\n");
-    }
-    return data[row][col];
-}
-
-// Print Matrix
-template <size_t Rows, size_t Cols>
-void Matrix<Rows, Cols>::print() const {
-    for(const auto& row : data) {
-        for(const auto& element : row) {
-            std::cout << element << " ";
-        }
-        std::cout#include "Matrix.hpp"
-
 int main() {
     Matrix<2, 3> mat1;
     mat1.set(0, 0, 1);
@@ -80,6 +45,7 @@ int main() {
     std::cout << "Matrix 3:\n";
     mat3.print();
     
+    // Matrix Product (Matrix 1 * Matrix 3)
     Matrix<2, 2> matProduct = mat1.multiply(mat3);
     std::cout << "Matrix Product (Matrix 1 * Matrix 3):\n";
     matProduct.print();
@@ -94,54 +60,7 @@ int main() {
     std::cout << "Matrix 4:\n";
     mat4.print();
     
-    std::cout << "Determinant of Matrix:\n" << mat4.determinant() << '\n';
+    std::cout << "Determinant of Matrix 4:\n" << mat4.determinant() << '\n';
 
     return 0;
-} << '\n';
-    }
 }
-
-// Matrix Addition
-template <size_t Rows, size_t Cols>
-Matrix<Rows, Cols> Matrix<Rows, Cols>::add(const Matrix<Rows, Cols>& other) const {
-    Matrix<Rows, Cols> result;
-    for(size_t i = 0; i < Rows; ++i) {
-        for(size_t j = 0; j < Cols; ++j) {
-            result.set(i, j, this->get(i, j) + other.get(i, j));
-        }
-    }
-
-    return result;
-}
-
-// Matrix Multiplication
-template <size_t Rows, size_t Cols>
-template <size_t OtherCols>
-Matrix<Rows, OtherCols> Matrix<Rows, Cols>::multiply(const Matrix<Rows, OtherCols>& other) const {
-    Matrix<Rows, OtherCols> result;
-    for(size_t i = 0; i < Rows; ++i) {
-        for(size_t j = 0; j < OtherCols; ++j) {
-            int sum = 0;
-            for(size_t k = 0; k < Cols; ++k) {
-                sum += this->get(i, k) * other.get(k, j);
-            }
-
-            result.set(i, j, sum);
-        }
-    }
-
-    return result;
-}
-
-// Matrix Transponse
-template <size_t Rows, size_t Cols>
-Matrix<Cols, Rows> Matrix<Rows, Cols>::transpose() const {
-    Matrix<Cols, Rows> result;
-    for(size_t i = 0; i < Rows; ++i) {
-        for(size_t j = 0; j < Cols; ++j) {
-            result.set(j, i, this->get(i, j));
-        }
-    }
-    return result;
-}
-
--- FocusOnCppTopics/MatrixCalculator/Matrix.hpp
+++ FocusOnCppTopics/MatrixCalculator/Matrix.hpp
@@ -22,14 +22,14 @@ public:
     // Set and get functions
     void set(size_t row, size_t col, int value) {
         if (row >= Rows || col >= Cols) {
-            throw std::out_of_range("Index out of range\n");
+            throw std::out_of_range("Index out of range");
         }
         data[row][col] = value;
     }
 
     int get(size_t row, size_t col) const {
         if (row >= Rows || col >= Cols) {
-            throw std::out_of_range("Index out of range\n");
+            throw std::out_of_range("Index out of range");
         }
         return data[row][col];
     }
@@ -95,20 +95,20 @@ long Matrix<1, 1>::determinant() const {
 // Specialization for 2x2 determinant
 template<>
 long Matrix<2, 2>::determinant() const {
-    return data[0][0]*data[1][1] - data[0][1] * data[1][0];
+    return data[0][0] * data[1][1] - data[0][1] * data[1][0];
 }
 
 // General determinant implementation
 template <size_t Rows, size_t Cols>
 long Matrix<Rows, Cols>::determinant() const {
-    static_assert(Rows == Cols, "Determinant is only defined for square matrices\n");
+    static_assert(Rows == Cols, "Determinant is only defined for square matrices");
 
     long det = 0;
-    for(size_t j = 0; j < Cols; ++j) {
-        if(Rows > 2) {
+    for (size_t j = 0; j < Cols; ++j) {
+        if (Rows > 2) {
             Matrix<Rows - 1, Cols - 1> submatrix;
-            for(size_t sub_i = 0; sub_i < Rows - 1; ++sub_i) {
-                for(size_t sub_j = 0; sub_j < Cols - 1; ++sub_j) {
+            for (size_t sub_i = 0; sub_i < Rows - 1; ++sub_i) {
+                for (size_t sub_j = 0; sub_j < Cols - 1; ++sub_j) {
                     submatrix.set(sub_i, sub_j, this->get(sub_i + 1, (sub_j >= j ? sub_j + 1 : sub_j)));
                 }
             }
