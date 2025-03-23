7e452af34e583cc0d0400c5f404715053fb34665
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2025-03-23 11:22:42 +0530

    Update few functions

--- PosixStandardShell/pshell.hpp
+++ PosixStandardShell/pshell.hpp
@@ -6,7 +6,13 @@
 #include <vector>
 #include <sstream>
 #include <unistd.h>
+#include <fstream>
+#include <sys/wait.h>
 
 void start_shell();
+std::vector<std::string> split_string(const std::string& str, char delimeter);
+std::vector<std::string> get_path_dirs();
+void handle_builtin_commands(const std::vector<std::string>& arguments, const std::vector<std::string>& path_dirs);
+void execute_external_command(const std::vector<std::string>& arguments, const std::vector<std::string>& path_dirs);
 
 #endif // PSHELL_HPP
